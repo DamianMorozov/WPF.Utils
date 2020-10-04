d77695321686369c1893ce130d774b3b5d358a39
Damian Viktorovich Morozov <damian@morozov33.ru>
2020-10-04 17:16:21 +0300

    Added WPF.Utils project

---
+++ WPF.Utils/Views/UserControlAbout.xaml.cs
@@ -0,0 +1,15 @@
+﻿// ReSharper disable UnusedMember.Global
+
+namespace WPF.Utils.Views
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControlAbout
+    {
+        public UserControlAbout()
+        {
+            InitializeComponent();
+        }
+    }
+}
