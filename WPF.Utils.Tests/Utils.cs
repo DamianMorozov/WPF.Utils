37aa91ad16d66bd8da8db0214c3573309d8d649a
Damian Viktorovich Morozov <damian@morozov33.ru>
2020-10-17 15:04:49 +0300

    Added Utils

---
+++ WPF.Utils.Tests/Utils.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System.Runtime.CompilerServices;
+
+namespace WPF.Utils.Tests
+{
+    /// <summary>
+    /// Utilites.
+    /// </summary>
+    public static class Utils
+    {
+        internal static void MethodStart([CallerMemberName] string memberName = "")
+        {
+            TestContext.WriteLine(@"--------------------------------------------------------------------------------");
+            TestContext.WriteLine($@"{memberName} start.");
+        }
+
+        internal static void MethodComplete([CallerMemberName] string memberName = "")
+        {
+            TestContext.WriteLine($@"{memberName} complete.");
+        }
+    }
+}
