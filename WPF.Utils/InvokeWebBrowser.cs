02076dbbaeaa683ee884c31d04c38692acc1bc20
Damian Viktorovich Morozov <damian@morozov33.ru>
2020-10-21 23:11:57 +0300

    Added InvokeWebBrowser

---
+++ WPF.Utils/InvokeWebBrowser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace WPF.Utils
+{
+    public static class InvokeWebBrowser
+    {
+        public static void SetSource(WebBrowser browser, Uri uri)
+        {
+            if (browser is null)
+                return;
+            void Work(WebBrowser inBrowser, Uri inUri)
+            {
+                inBrowser.Source = inUri;
+            }
+            if (!browser.CheckAccess())
+                if (!(System.Windows.Application.Current is null))
+                    System.Windows.Application.Current.Dispatcher.Invoke(DispatcherPriority.Normal, new Action(() =>
+                    {
+                        Work(browser, uri);
+                    }));
+                else
+                    browser.Dispatcher.Invoke(DispatcherPriority.Normal, new Action(() =>
+                    {
+                        Work(browser, uri);
+                    }));
+            else
+                Work(browser, uri);
+        }
+    }
+}
